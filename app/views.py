12ed4156ff7756758807ba30748bf84a0053fe35
edugarcia98 <edugarcia758@gmail.com>
2020-09-27 10:29:05 -0300

    Correção pós deploy

--- app/views.py
+++ app/views.py
@@ -9,16 +9,16 @@ def index(request):
     consultas = Consulta.objects.order_by('numero_guia')
 
     filter_medico = request.GET.get('filter_medico')
-    if filter_medico != '':
+    if (filter_medico != '') and (filter_medico != None):
         consultas = consultas.filter(medico__nome_medico__iexact=filter_medico)
 
     filter_periodo_inicial = request.GET.get('filter_periodo_inicial')
-    if filter_periodo_inicial != '':
+    if (filter_periodo_inicial != '') and (filter_periodo_inicial != None):
         filter_periodo_inicial = datetime.datetime.strptime(filter_periodo_inicial, '%Y-%m-%d')
         consultas = consultas.filter(data_consulta__gte=filter_periodo_inicial)
 
     filter_periodo_final = request.GET.get('filter_periodo_final')
-    if filter_periodo_final != '':
+    if filter_periodo_final != '' and (filter_periodo_final != None):
         filter_periodo_final = datetime.datetime.strptime(filter_periodo_final, '%Y-%m-%d')
         consultas = consultas.filter(data_consulta__lte=filter_periodo_final)
 
